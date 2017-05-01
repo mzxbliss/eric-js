73b53637f9acffe1582d5a33a8205d38f3e0ca86
Emma Nguyen <t.emmanguyen@gmail.com>
2017-05-01 15:32:59 +0000

    exercises for may 1

--- javascript_exercises/short_exercises.js
+++ javascript_exercises/short_exercises.js
@@ -3,7 +3,7 @@
 //1. Array item access
 
 var arr = [1,2,3,4,5,6,7,8]; 
-arr[/*...*/] // What goes between the square brackets if I want the value 6? 
+arr[5] // What goes between the square brackets if I want the value 6? 
 
 
 //2. Object value access
@@ -15,7 +15,7 @@ var fateOfTheFurious = { //How do I get the budget from this object?
     duration: "136 minutes"
 }
 
-fateOfTheFurious/*...*/
+fateOfTheFurious["budget"]; //to get bedget value
 
 /*3. Write a function that takes a name with the following format 
 "FIRST MIDDLE_INITIAL LAST" and outputs it as "LAST, FIRST MIDDLE_INITIAL" 
